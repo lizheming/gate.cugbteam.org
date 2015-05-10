fe91e2486abd03577d5939dbe2f5d9597f2ba202
lizheming <i@imnerd.org>
2015-05-10 21:49:35 +0800

    update shell

--- update/update.sh
+++ update/update.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 echo "Start deployment"
 cd /usr/nginx/html/gate
-su osms
-git reset --hard origin/master
-git clean -f
-echo "pulling source code..."
-git pull origin master
+sudo -u osms -H git pull origin master --force
 echo "Finished"
-exit
-
- 
-WEB_PATH='/var/www/dev.lovelucy.info'
-WEB_USER='lovelucydev'
-WEB_USERGROUP='lovelucydev'
- 
-echo "Start deployment"
-cd $WEB_PATH
-echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
-git pull
-git checkout master
-echo "changing permissions..."
-chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
-echo "Finished."
